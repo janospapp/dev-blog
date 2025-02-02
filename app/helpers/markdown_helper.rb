a4f05dcc8d253b75dac8fe2230f5e63d1de65114
Janos Papp <papp.janos.90@gmail.com>
2025-02-02 23:40:02 +0100

    Add styling with highlight.js

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,18 +1,28 @@
 module MarkdownHelper
   def md(text)
-    renderer.render(text).html_safe
+    markdown.render(text).html_safe
   end
 
   private
 
+  def markdown
+    @markdown ||= Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
+  end
+
+  class CustomRenderer < Redcarpet::Render::HTML
+    def block_code(code, language)
+      %(<pre class="
+          my-5 border-solid border-2 border-nord8 rounded-md
+          shadow-md shadow-nord8"
+        ><code class="language-#{language}">#{code}</code></pre>)
+    end
+  end
+
   def renderer
-    @renderer ||= Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML,
+    @renderer ||= CustomRenderer.new(
       autolink: true,
       filter_html: true,
-      with_toc_data: true,
-      prettify: true,
-      fenced_code_blocks: true
+      with_toc_data: true
     )
   end
 end
